7f979629fb9f5082e615890686f38fba57cb5ad7
Amaan Vora <43636138+deadven7@users.noreply.github.com>
2021-05-08 12:10:56 +0530

    Add files via upload

---
+++ checkers/constants.py
@@ -0,0 +1,14 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLS = 8, 8
+SQUARE_SIZE = WIDTH//COLS
+
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (85, 107, 47)
+BLUE = (0, 0, 255)
+
+CROWN = pygame.transform.scale(pygame.image.load('crown.png'), (30, 15))
+
